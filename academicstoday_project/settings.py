692c951f36d3c977fc65f9d03fea247e1efcb3d7
EnockOMONDI <enockomondike@gmail.com>
2023-10-03 05:31:17 +0300

    configured gunicorn

--- academicstoday_project/settings.py
+++ academicstoday_project/settings.py
@@ -207,4 +207,3 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
     # Add to this list all the locations containing your static files 
 )
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
